59629459551cad21bd65d8b93d420e9bd4561233
Joshua Zapata <hello@joshuazapata.com>
2018-04-02 23:22:53 -0700

    add assets and views

---
+++ assets/javascripts/application.js
@@ -0,0 +1,39 @@
+// This is where you should bootstrap your JS app
+//= require jquery
+//= require jquery_ujs
+//= require jquery.turbolinks
+//= require scaffold
+//= require dropzone
+//= require bootstrap
+//= require abracadabra
+//= require messages
+//= require_self
+//= require_tree .
+
+var mouseover_search;
+mouseover_search = function() {
+
+  $(".unmoused-search").show();
+  $(".moused-search").hide();
+
+  $(".unmoused-search").mouseenter(function() {
+      $(".moused-search").show();
+      $(".unmoused-search").hide();
+      $(this).hide();
+  });
+
+  $(".moused-search").mouseout(function() {
+    $(".unmoused-search").show();
+    $(".moused-search").hide();
+    $(this).hide();
+  });
+};
+
+$(document).ready(mouseover_search);
+
+header_set = function() {
+  firstHeight = $("#header").height() + 'px';
+  $("#header-div").css("height", firstHeight);
+};
+
+$(document).ready(header_set);
---
+++ assets/javascripts/messages.coffee
@@ -0,0 +1,8 @@
+//= require chosen-jquery
+
+$ ->
+  # enable chosen js
+  $('.chosen-it').chosen
+    allow_single_deselect: true
+    no_results_text: 'No results matched'
+    width: '200px'
